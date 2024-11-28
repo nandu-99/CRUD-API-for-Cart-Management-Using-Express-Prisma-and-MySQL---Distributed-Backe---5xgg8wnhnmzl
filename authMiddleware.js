3e6b34c6ca342ba82c67b1b6083f0fd38ef1c225
Pottabathini Vivekananda <vivekananda.99666@gmail.com>
2024-11-28 22:17:17 +0530

    made changes

--- authMiddleware.js
+++ authMiddleware.js
@@ -2,7 +2,7 @@ const authMiddleware = (req, res, next)=>{
     const authKey = "8a60348b-d4a4-564a-9b45-aab518adb7f4"
     const {apiauthkey} = req.headers; 
     if(!apiauthkey){
-        return res.status(403).json({ error: 'API authentication key is required.' });
+        return res.status(403).json({ error: 'apiauthkey is missing or invalid' });
     }
     if (apiauthkey !== authKey) {
         return res.status(403).json({ error: 'Failed to authenticate apiauthkey' });
